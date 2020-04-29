a2834da73847d30cf4dfa2c80fe0ff910846c208
Sam O <olukotun.ts@gmail.com>
2020-04-29 17:34:53 -0600

    Fix command

--- internal/junitxml/report.go
+++ internal/junitxml/report.go
@@ -180,7 +180,7 @@ func newJUnitTestCase(tc testjson.TestCase, formatClassname FormatFunc) JUnitTes
 	return JUnitTestCase{
 		Classname: formatClassname(tc.Package),
 		Name:      tc.Test,
-		File:	   format(Classname(tc.Package)),
+		File:	   formatClassname(tc.Package),
 		Time:      formatDurationAsSeconds(tc.Elapsed),
 	}
 }
