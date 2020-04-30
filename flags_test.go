3615ab00000aa732c89a8fab443b8ad876ea3195
Sam O <olukotun.ts@gmail.com>
2020-04-29 19:23:14 -0600

    Increase parallelism. Add delay to tests

--- flags_test.go
+++ flags_test.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"gotest.tools/v3/assert"
 )
 
 func TestNoSummaryValue_SetAndString(t *testing.T) {
 	t.Run("none", func(t *testing.T) {
+		time.Sleep(5 * time.Second)
 		assert.Equal(t, newNoSummaryValue().String(), "none")
 	})
 	t.Run("one", func(t *testing.T) {
@@ -16,11 +18,13 @@ func TestNoSummaryValue_SetAndString(t *testing.T) {
 		assert.Equal(t, value.String(), "output")
 	})
 	t.Run("some", func(t *testing.T) {
+		time.Sleep(5 * time.Second)
 		value := newNoSummaryValue()
 		assert.NilError(t, value.Set("errors,failed"))
 		assert.Equal(t, value.String(), "failed,errors")
 	})
 	t.Run("bad value", func(t *testing.T) {
+		time.Sleep(5 * time.Second)
 		value := newNoSummaryValue()
 		assert.ErrorContains(t, value.Set("bogus"), "must be one or more of")
 	})
